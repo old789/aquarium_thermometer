09363ae17c5fd2dbbc066cec453330b6dff779c8
old789 <old789@users.noreply.github.com>
2025-05-08 22:11:26 +0300

    more works
    Text messages placed in a flash and  extended logging

--- aquarium_thermometer.ino
+++ aquarium_thermometer.ino
@@ -15,36 +15,64 @@ DS18B20 ds2(&oneWire2);
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
 
+#define HALT while(true){ delay(100); }
+
+double t1, t2;
+
 void setup() {
   int status;
-
+  bool sensor1_fail=true, sensor2_fail=true;
+  PGM_P msg_booting=PSTR("\r\nBooting...");
+  PGM_P msg_lcd_fail=PSTR("LCD initialise failed, status = ");
+  PGM_P msg_sensor1 = PSTR("sensor 1 ");
+  PGM_P msg_sensor2 = PSTR("sensor 2 ");
+  PGM_P msg_fail = PSTR("fail");
+  PGM_P msg_ok = PSTR("Ok!");
+  // PGM_P msg_ = PSTR();
+  // PGM_P msg_ = PSTR();
+  // PGM_P msg_ = PSTR();
+ 
   Serial.begin(115200);
   delay(100);
-  Serial.println("\r\nBooting...");
+  Serial.println(FPSTR(msg_booting));
 
   status=lcd.begin(LCD_COLS, LCD_ROWS);
   if(status){
-    Serial.printf("LCD initialise failed, status = %d\r\n", status);
-    while(true){ delay(100); }
+    Serial.print(FPSTR(msg_lcd_fail));
+    Serial.println(status);
+    HALT
   }
 
   lcd.clear();
+  lcd.print(FPSTR(msg_sensor1));
+  Serial.print(FPSTR(msg_sensor1));
   if (ds1.begin() == false){
-    lcd.print("sensor 1 fail"); 
+    lcd.print(FPSTR(msg_fail)); 
+    Serial.println(FPSTR(msg_fail)); 
   }else{
-    lcd.print("sensor 1 Ok!"); 
+    sensor1_fail=false;
+    lcd.print(FPSTR(msg_ok)); 
+    Serial.println(FPSTR(msg_ok)); 
   }
   
   lcd.setCursor(0,1);
-
+  lcd.print(FPSTR(msg_sensor2));
+  Serial.print(FPSTR(msg_sensor2));
   if (ds2.begin() == false){
-    lcd.print("sensor 2 fail"); 
+    lcd.print(FPSTR(msg_fail)); 
+    Serial.println(FPSTR(msg_fail)); 
   }else{
-    lcd.print("sensor 2 Ok!"); 
+    sensor2_fail=false;
+    lcd.print(FPSTR(msg_ok)); 
+    Serial.println(FPSTR(msg_ok)); 
   }
   
-  ds1.setResolution(10);
-  ds2.setResolution(10);
+  if ( sensor1_fail or sensor2_fail ) {
+    HALT
+  }
+
+  ds1.setResolution(12);
+  ds2.setResolution(12);
 
   ds1.requestTemperatures();
   ds2.requestTemperatures();
@@ -53,20 +81,25 @@ void setup() {
 }
 
 void loop() {
+  PGM_P msg_t1 = PSTR("Aqua t = ");
+  PGM_P msg_t2 = PSTR("Room t = ");
+  PGM_P msg_spacer = PSTR("  ");
+  // PGM_P msg_ = PSTR();
   if (ds1.isConversionComplete()){
+    t1=ds1.getTempC();
     lcd.setCursor(0,0);
-    lcd.print("t1 = ");
-    lcd.print(ds1.getTempC(),1);
-    lcd.print("  ");
+    lcd.print(FPSTR(msg_t1));
+    lcd.print(t1,1);
+    lcd.print(msg_spacer);
     ds1.requestTemperatures();
   }
   if (ds2.isConversionComplete()){
+    t2=ds2.getTempC();
     lcd.setCursor(0,1);
-    lcd.print("t2 = ");
-    lcd.print(ds2.getTempC(),1);
-    lcd.print("  ");
+    lcd.print(msg_t2);
+    lcd.print(t2,1);
+    lcd.print(msg_spacer);
     ds2.requestTemperatures();
   }
-  //lcd.clear();
   delay(60000);
 }
