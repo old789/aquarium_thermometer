57a9051a8a71098788bba9a6538664eaa9883d36
old789 <old789@users.noreply.github.com>
2025-05-28 18:09:46 +0300

    Update cli.ino

--- cli.ino
+++ cli.ino
@@ -30,11 +30,11 @@ void set_name(Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
-  //terminal->println();
+  terminal->println();
   if ( passed ) {
     terminal->println(PASSED, "Device name set to \"" + String(dev_name) + "\"");
   } else {
@@ -45,11 +45,124 @@ void set_name(Terminal* terminal) {
 
 void set_network_mode (Terminal* terminal) {
   bool passed = false;
+  int i;
   String value = terminal->readParameter();
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    i = value.toInt();
+    if ( i == 0 or i == 1) {
+      network_enable = (bool)i; 
+      passed = true;
+    } else {
+      terminal->println(ERROR, "Parameter " + value + " is not between 0 or 1!");
+    }
+  }
+  terminal->println();
+  if ( passed ) {
+    if (network_enable) {
+      terminal->println(PASSED, "Network mode enabled");
+    } else {
+      terminal->println(PASSED, "Network mode disabled");
+    }
+  } else {
+    terminal->println(FAILED, "Network mode unchanged");
+  }
+  terminal->prompt();
+}
+
+void set_ssid (Terminal* terminal) {
+  bool passed = false;
+  String value = terminal->readParameter();
+  if (value == NULL) {
+    terminal->invalidParameter();
+  } else {
+    memset(ssid, 0, sizeof(ssid));
+    value.toCharArray(ssid, sizeof(ssid)-1);
+    passed = true;
+  }
+  terminal->println();
+  if ( passed ) {
+    terminal->println(PASSED, "SSID set to \"" + String(ssid) + "\"");
+  } else {
+    terminal->println(FAILED, "SSID not set");
+  }
+  terminal->prompt();
+}
+
+void set_passw (Terminal* terminal) {
+  bool passed = false;
+  String value = terminal->readParameter();
+  if (value == NULL) {
+    terminal->invalidParameter();
+  } else {
+    memset(passw, 0, sizeof(passw));
+    value.toCharArray(passw, sizeof(passw)-1 );
+    passed = true;
+  }
+  terminal->println();
+  if ( passed ) {
+    terminal->println(PASSED, "WiFi password set to \"" + String(passw) + "\"");
+  } else {
+    terminal->println(FAILED, "WiFi password not set");
+  }
+  terminal->prompt();
+}
+
+void set_dns (Terminal* terminal) {
+  bool passed = false;
+  int i;
+  String value = terminal->readParameter();
+  if (value == NULL) {
+    terminal->invalidParameter();
+  } else {
+    i = value.toInt();
+    if ( i == 0 or i == 1) {
+      mqtt_host_resolving = i;
+      passed = true;
+    } else {
+      terminal->println(ERROR, "Parameter " + value + " is not between 0 or 1!");
+    }
+  }
+  terminal->println();
+  if ( passed ) {
+    if (mqtt_host_resolving == 0) {
+      terminal->println(PASSED, "Resolving mode set to mDNS");
+    } else {
+      terminal->println(PASSED, "Resolving mode set to DNS");
+    }
+  } else {
+    terminal->println(FAILED, "Resolving mode unchanged");
+  }
+  terminal->prompt();
+}
+
+void set_host (Terminal* terminal) {
+  bool passed = false;
+  String value = terminal->readParameter();
+  if (value == NULL) {
+    terminal->invalidParameter();
+  } else {
+    memset(host, 0, sizeof(host));
+    value.toCharArray(host, sizeof(host)-1 );
+    passed = true;
+  }
+  terminal->println();
+  if ( passed ) {
+    terminal->println(PASSED, "Hostname set to \"" + String(host) + "\"");
+  } else {
+    terminal->println(FAILED, "Hostname not set");
+  }
+  terminal->prompt();
+}
+
+void set_ (Terminal* terminal) {
+  bool passed = false;
+  String value = terminal->readParameter();
+  if (value == NULL) {
+    terminal->invalidParameter();
+  } else {
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -68,7 +181,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -87,7 +200,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -106,7 +219,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -125,7 +238,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -144,7 +257,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
@@ -163,83 +276,7 @@ void set_ (Terminal* terminal) {
   if (value == NULL) {
     terminal->invalidParameter();
   } else {
-    memset(ups_name, 0, sizeof(ups_name));
-    value.toCharArray(dev_name, sizeof(dev_name)-1 );
-    passed = true;
-  }
-  terminal->println();
-  if ( passed ) {
-    terminal->println(PASSED, "Device name set to \"" + String(dev_name) + "\"");
-  } else {
-    terminal->println(FAILED, "Device name not set");
-  }
-  terminal->prompt();
-}
-
-void set_ (Terminal* terminal) {
-  bool passed = false;
-  String value = terminal->readParameter();
-  if (value == NULL) {
-    terminal->invalidParameter();
-  } else {
-    memset(ups_name, 0, sizeof(ups_name));
-    value.toCharArray(dev_name, sizeof(dev_name)-1 );
-    passed = true;
-  }
-  terminal->println();
-  if ( passed ) {
-    terminal->println(PASSED, "Device name set to \"" + String(dev_name) + "\"");
-  } else {
-    terminal->println(FAILED, "Device name not set");
-  }
-  terminal->prompt();
-}
-
-void set_ (Terminal* terminal) {
-  bool passed = false;
-  String value = terminal->readParameter();
-  if (value == NULL) {
-    terminal->invalidParameter();
-  } else {
-    memset(ups_name, 0, sizeof(ups_name));
-    value.toCharArray(dev_name, sizeof(dev_name)-1 );
-    passed = true;
-  }
-  terminal->println();
-  if ( passed ) {
-    terminal->println(PASSED, "Device name set to \"" + String(dev_name) + "\"");
-  } else {
-    terminal->println(FAILED, "Device name not set");
-  }
-  terminal->prompt();
-}
-
-void set_ (Terminal* terminal) {
-  bool passed = false;
-  String value = terminal->readParameter();
-  if (value == NULL) {
-    terminal->invalidParameter();
-  } else {
-    memset(ups_name, 0, sizeof(ups_name));
-    value.toCharArray(dev_name, sizeof(dev_name)-1 );
-    passed = true;
-  }
-  terminal->println();
-  if ( passed ) {
-    terminal->println(PASSED, "Device name set to \"" + String(dev_name) + "\"");
-  } else {
-    terminal->println(FAILED, "Device name not set");
-  }
-  terminal->prompt();
-}
-
-void set_ (Terminal* terminal) {
-  bool passed = false;
-  String value = terminal->readParameter();
-  if (value == NULL) {
-    terminal->invalidParameter();
-  } else {
-    memset(ups_name, 0, sizeof(ups_name));
+    memset(dev_name, 0, sizeof(dev_name));
     value.toCharArray(dev_name, sizeof(dev_name)-1 );
     passed = true;
   }
